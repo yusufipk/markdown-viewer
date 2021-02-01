9faa79f6ac8332ea3225be96f8da493f9caa3928
Yusuf İpek <yusufipek@protonmail.com>
2021-02-01 08:21:28 +0300

    fixed error step 6

--- src/components/text-area/text-area-component.jsx
+++ src/components/text-area/text-area-component.jsx
@@ -6,7 +6,7 @@ class TextArea extends Component {
     super(props);
     this.state = {
       input:
-        " # Write your markdown here! \r\n ## or here \r\n [hey](google.com) \r\n ```javascript \r\n var s = 'JavaScript syntax highlighting'; \r\n alert(s); \r\n ``` \r\n Inline `code` has `back-ticks around` it. \r\n 1. First ordered list item \r\n 2. Another item \r\n ⋅⋅* Unordered sub-list. \r\n1. Actual numbers don't matter, just that it's a number \r\n ⋅⋅1. Ordered sub-list \r\n 4. And another item. \r\n > This is a very long line that will still be quoted properly when it wraps. Oh boy let's keep writing to make sure this is long enough to actually wrap for everyone. Oh, you can *put* **Markdown** into a blockquote.  \r\n ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png 'Logo Title Text 1')",
+        " # Write your markdown here! \n ## or here \n [hey](google.com) \n ```javascript \n var s = 'JavaScript syntax highlighting'; \n alert(s); \n ``` \n Inline `code` has `back-ticks around` it. \n 1. First ordered list item \n 2. Another item \n ⋅⋅* Unordered sub-list. \n1. Actual numbers don't matter, just that it's a number \n ⋅⋅1. Ordered sub-list \n 4. And another item. \n > This is a very long line that will still be quoted properly when it wraps. Oh boy let's keep writing to make sure this is long enough to actually wrap for everyone. Oh, you can *put* **Markdown** into a blockquote.  \n ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png 'Logo Title Text 1')",
     };
   }
 
